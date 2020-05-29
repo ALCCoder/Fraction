cbb5cf1fdb9dd8a8cb99442a151e55234b6e4f58
ALCCoder <44910827+ALCCoder@users.noreply.github.com>
2020-05-29 23:29:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Fraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Fraction a = new Fraction(1, 2);
+                Fraction b = new Fraction(1, 2);
+            
+
+                Console.WriteLine(a + b);
+                Console.WriteLine(a - b);
+                Console.WriteLine(a.Equals(b));
+                Console.WriteLine(a * b);
+                Console.WriteLine(a / b);
+                Console.WriteLine(a > b);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e);
+
+            }
+
+        }
+    }
+}
